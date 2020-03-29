6ff07197d09d3bf1a5c5db120e81e38d516e9882
Adeeb Shihadeh <adeebshihadeh@gmail.com>
2020-03-29 13:04:45 -0700

    alacritty config

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ DIR=$(dirname $(realpath $0))
 ln -s $DIR/sshconfig $HOME/.ssh/config
 ln -s $DIR/.screenrc $HOME
 ln -s $DIR/.vimrc $HOME
+ln -s $DIR/.alacritty.yml $HOME
 ln -s $DIR/.i3 $HOME/.config/i3/config
 ln -s $DIR/status.toml $HOME/.config/i3/status.toml
-
 
